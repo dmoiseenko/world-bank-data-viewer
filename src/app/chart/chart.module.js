9119cc6f88a6c693c3f0303e182d13a1ee212d89
lampas21 <lampas.odesk@gmail.com>
2015-11-10 09:55:14 +0300

    Remove empty strings

--- src/app/chart/chart.module.js
+++ src/app/chart/chart.module.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 module.exports = angular.module('app.chart', ['n3-line-chart'])
     .directive('chart', require('./chart.directive'))
     .directive('indicatorInfo', require('./indicatorInfo.directive'))
