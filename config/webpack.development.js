8812ef8fee48c519ce642439b82e147a74be9acb
lampas21 <lampas.odesk@gmail.com>
2016-01-19 18:16:55 +0300

    Fix typo

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -9,7 +9,7 @@ var config = require('./webpack.base.js');
 config.output.path = path.resolve(__dirname, '../.build');
 
 config.plugins.push(new ExtractTextPlugin('[name].css', { disable: true }));
-config.plugins.push(new new webpack.DefinePlugin({ NODE_ENV: JSON.stringify('production') }));
+config.plugins.push(new webpack.DefinePlugin({ NODE_ENV: JSON.stringify('production') }));
 config.plugins.push(new webpack.optimize.CommonsChunkPlugin('vendors', 'vendor.js'));
 
 config.devtool = 'cheap-inline-module-source-map';
